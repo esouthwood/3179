932dd46eddf8e7849cacfc74c70efc0959329278
esouthwood <uqz19ghu@uea.ac.uk>
2022-10-17 14:35:34 +0800

    Update Layour

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,6 +3,7 @@ var vg_2 = "production2.vg.json";
 var vg_3 = "solarconsumtions2.vg.json";
 var vg_4 = "choropleth_time_slider.vg.json";
 var vg_5 = "bar_chart.vg.json";
+var vg_6 = "linechar.vg.json"
 
 vegaEmbed("#multi_view", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -21,5 +22,9 @@ vegaEmbed("#chloropleth_time_slider", vg_4).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#bar_chart", vg_5).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#line_chart", vg_6).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
